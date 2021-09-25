956a992dad159e92d6f879ab73f514daca543be1
Clemens Punzengruber <clemens.punzengruber@rise-world.com>
2021-09-25 20:24:14 +0200

    switch lock and minus on win layer

--- keyboards/ergodox_ez/keymaps/rince83/keymap.c
+++ keyboards/ergodox_ez/keymaps/rince83/keymap.c
@@ -143,9 +143,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 RGB_TOG,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPACE,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRACKET,                                    KC_RBRACKET,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINUS,
     KC_LCTRL,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
-    KC_LSPO,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_MINUS,                                       KC_EQUAL,       KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,
+    KC_LSPO,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_LOCK,                                       KC_EQUAL,       KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,
     KC_GRAVE,       KC_LALT,        KC_LGUI,        KC_LEFT,        KC_RIGHT,                                                                                                       KC_DOWN,        KC_UP,          KC_RGUI,        KC_RALT,        KC_TRANSPARENT,
-                                                                                                    KC_LOCK,        KC_LALT,              KC_DELETE,      KC_ESCAPE,
+                                                                                                    KC_MINUS,        KC_LALT,              KC_DELETE,      KC_ESCAPE,
                                                                                                                     KC_LGUI,              KC_TRANSPARENT,
                                                                                     KC_SPACE,       KC_BSPACE,      TT(WIN_SYM),          KC_TRANSPARENT,          KC_BSPACE,          KC_ENTER
 ),
